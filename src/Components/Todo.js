f92298cc4ed5f5ba70be1e2f10ab8b5f6fa6f790
faiz260 <59044640+faiz260@users.noreply.github.com>
2020-06-13 14:31:43 +0100

    Material UI added

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import {TextField, Button} from '@material-ui/core/';
 
 class Todo extends Component {
     state = {
         text: "",
         todoItem: []
     }
-
     handlerChange = (e) => {
         this.setState({
             text: e.target.value
@@ -18,27 +18,30 @@ class Todo extends Component {
             todoItem: mergedTodos,
             text: ""
         })
+        // console.log(`Item ${id} added`)
     }
     delItem = (e) => {
         let id = e.target.id;
         this.state.todoItem.splice(id, 1);
         this.setState({ todoItem: this.state.todoItem })
+        console.log(e.target.id)
     }
     render() {
         console.log(this.state)
         return (
             <div>
                 <h1>Todo App</h1>
-                <form onSubmit={this.addHandler}>
-                    <input type="text" value={this.state.text} onChange={this.handlerChange}></input>
-                    <input type="submit"></input>
+                <form >
+                    {/* <input type="text" value={this.state.text} onChange={this.handlerChange}></input> */}
+                    <TextField  width="10" id="filled-basic" label="Enter your todo" variant="filled" value={this.state.text} onChange={this.handlerChange} />
+                    <Button variant="contained" onClick={this.addHandler} >Add</Button>
+                    {/* <input type="submit"></input> */}
                 </form>
                 {this.state.todoItem.map((el, i) => {
                     return (
-                        <li key={i}>{el} <span id={i} onClick={this.delItem}><button >Delete</button></span> </li>
+                        <li key={i}>{el} <span id={i} onClick={this.delItem}> <Button variant="contained" color="secondary" >Delete</Button></span> </li>
                     )
                 })
-
                 }
             </div>
         )
